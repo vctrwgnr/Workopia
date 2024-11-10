5706fc6fb6df8398a6c843541051415b9331d3fc
vctrwgnr <victoriaemich@gmail.com>
2024-11-10 18:29:57 +0100

    Add partials

--- helpers.php
+++ helpers.php
@@ -4,3 +4,41 @@ function basePath($path = '') {
     return __DIR__ . '/' . $path;
 
 }
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View '{$name} not found!'";
+    }
+}
+
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial '{$name} not found!'";
+    }
+}
+
+/**
+ * Inspect a value(s) (and kill the script after)
+ * @param mixed $value
+ * @return void
+ */
+
+function inspect($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+
+}
+
+function inspectAndDie($value){
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
+}
